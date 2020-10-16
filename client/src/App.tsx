86fe9034770593152a2383ca9c81b53c7ddbc01e
Your Nam <you@example.com>
2020-10-15 20:42:54 -0500

    Project 1

---
+++ client/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo556.png';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Shanell Learns React
+        </p>
+        <a href="https://github.com/itdev161/shanell-spann2"
+          target="_blank" className="App-link"
+          rel="noopener noreferrer">
+            <button>Get Donuts</button>
+        </a>
+        
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
